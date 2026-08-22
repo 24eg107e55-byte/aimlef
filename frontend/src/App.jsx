ba9b43702f1c823d6fd4975993c3231e2fc0398c
vikramdev772 <vikramdev772@gmail.com>
2026-08-22 11:00:53 +0530

    router ⚙️

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import React, { useEffect, useState } from "react";
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import State from "./components/State";
 function App() {
- 
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/apple" element={<State/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
